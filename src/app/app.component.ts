25e1a699eda5679d02f023d8fe9365bcf7f8960b
avneet singh <avneet_cochhar@hotmail.com>
2018-05-30 20:48:23 -0600

    aync  api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-// import {AppService} from './app.service';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+// import { Observable } from 'rxjs/Rx';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
 
-  // constructor(private _appService: AppService) { }
-  ngOnInit() {}
-//   ngOnInit() {
-//     // var el = document.getElementById('contact');
-//     this._appService.sayHello()
-//     .subscribe(
-//     result => {
-//       alert(
-//         JSON.stringify(result));   
-     
-//     }
-//     );
-// }
+export class AppComponent  {
+
+
+
+
+// private greetUrl='https://company-look-up.herokuapp.com/list_pets';
+    private greetUrl='https://freegeoip.net/json/';
+data:any={};
+
+    constructor(private _http: Http) {
+this.getContacts();this.getData();
+
+     }
+
+getData(){
+  return this._http.get(this.greetUrl).map((res:Response)=>res.json())
+}
+getContacts(){
+  this.getData().subscribe(data=>{alert(data);this.data=data})
+
+}
+
+
 }
