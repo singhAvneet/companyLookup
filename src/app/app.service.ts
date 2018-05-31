275dd9a1cf0a0c6b38a8ca0926a168d21265d01f
avneet singh <avneet_cochhar@hotmail.com>
2018-05-30 22:49:44 -0600

    init

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,7 +10,8 @@ import 'rxjs/add/operator/catch';
 export class AppService {
     // private greetUrl = 'https://calm-ocean-26841.herokuapp.com/list_pets';
     // private greetUrl = 'https://singhavneet.000webhostapp.com/vote/api.php/?party=Conservative&month=may&email=';
-private greetUrl='https://company-look-up.herokuapp.com/list_pets';
+// private greetUrl='https://company-look-up.herokuapp.com/list_pets';
+private greetUrl='/list_pets';
     // private greetUrl='https://freegeoip.net/json/';
 
     // Resolve HTTP using the constructor
