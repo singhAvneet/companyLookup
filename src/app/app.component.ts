7eb20b471ec171f0a55d8ff42f026aa1d0c9c7f1
avneet singh <avneet_cochhar@hotmail.com>
2018-05-30 19:47:36 -0600

    aync  api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {AppService} from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private _appService: AppService) { }
+
+  ngOnInit() {
+    // var el = document.getElementById('contact');
+    this._appService.sayHello()
+    .subscribe(
+    result => {
+      alert(
+        JSON.stringify(result));   
+     
+    }
+    );
+}
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class AppService {
+    // private greetUrl = 'http://singhavneet.000webhostapp.com/vote/api.php/?party=Conservative&month=may&email=';
+private greetUrl='/list_pets';
+    // private greetUrl='https://freegeoip.net/json/';
+
+    // Resolve HTTP using the constructor
+    constructor(private _http: Http) {
+
+
+     }
+
+    sayHello(): Observable<any> {
+        return this._http.get(this.greetUrl).map((response: Response) => {
+            return response.json();
+        });
+    }
+
+  
+}
