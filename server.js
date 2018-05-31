645cfc0cf052b884769c5626d98eca834769ddaf
avneet singh <avneet_cochhar@hotmail.com>
2018-05-30 19:35:23 -0600

    aync  api

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var con = mysql.createPool({
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
